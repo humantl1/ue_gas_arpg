2449501e6f09b7fe8e9156d4d8b9e4490641b65d
human004 <timothybenton@gmail.com>
2025-04-17 08:38:51 -0700

    Add base ability system componenet infrastructure

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -3,20 +3,38 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
 #include "GameFramework/Character.h"
 #include "AuraCharacterBase.generated.h"
 
+class UAttributeSet;
+class UAbilitySystemComponent;
+
 UCLASS(Abstract)
-class AURA_API AAuraCharacterBase : public ACharacter
+class AURA_API AAuraCharacterBase : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 	AAuraCharacterBase();
 
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	FORCEINLINE UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 protected:
 	virtual void BeginPlay() override;
-
+	
+	// EditAnywhere allows this property to be modified in any instance
 	UPROPERTY(EditAnywhere, Category="Combat")
 	TObjectPtr<USkeletalMeshComponent> Weapon;
+	
+	// UPROPERTY() macro exposes variables to Unreal Engine's reflection system,
+	// enabling features like garbage collection, Blueprint access, and property replication
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	// TObjectPtr is the preferred way to hold UObject pointers in Unreal Engine.
+	// it automatically handles pointer dereferencing and null checks,
+	// making it safer than raw pointers while maintaining good performance.
+	UPROPERTY()
+	TObjectPtr<UAttributeSet> AttributeSet;
 };
